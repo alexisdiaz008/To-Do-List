56d5f728fd24cb1f29131988883a2bc3d0fe8e6c
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-11 22:56:36 -0400

    initial commit

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+
+// var main = function() {
+
+//   $('fa-check-square').click(function(){
+//   	$(this).toggleClass("active");
+//   });
+
+//   $('.fa-check-square').click(function(){
+//   	$(this).closest("p").remove();
+
+//   });
+
+
+// };
+
+// ICON JQUERY
